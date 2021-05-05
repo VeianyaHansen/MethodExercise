503722fcae04a8eb8e453392493c35bfb5a2e6a0
Veianya Hansen <melaya0105@gmail.com>
2021-05-05 08:04:12 -0700

    Add project files.

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            var favoriteBand = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}. Your favorite color is {favoriteColor}, your favorite animal is a {favoriteAnimal}, and you like listening to {favoriteBand}.");
+        }
+
+     
+    }
+}
